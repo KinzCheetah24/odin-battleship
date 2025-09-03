82805321ed508df05d79794a075f5965ce1489bb
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-09-03 02:16:09 +0200

    feat: domDisplay class added
    The domDisplay class was added for displaying the different elements on the page.

---
+++ src/domDisplay.js
@@ -0,0 +1,26 @@
+class DomDisplay {
+    constructor() {
+        this._realPlayerBoardDisplay = document.querySelector(".real-player-board-div");
+        this._computerPlayerBoardDisplay = document.querySelector(".computer-player-board-div");
+    }
+
+    displayPlayerBoard(type, board) {
+        let boardDisplay = null;
+        if(type === "real") {
+            boardDisplay = this._realPlayerBoardDisplay;
+        } else {
+            boardDisplay = this._computerPlayerBoardDisplay;
+        }
+
+        for(let i = 0 ; i < board.length ; i++) {
+            for (let j = 0 ; j < board[0].length ; j++) {
+                let square = document.createElement("div");
+                square.className = "board-square";
+
+                boardDisplay.appendChild(square);
+            }
+        }
+    }
+}
+
+module.exports = DomDisplay;
