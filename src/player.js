a5526d47cc84273726bc4a4e6b8aca3f23f89156
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-09-03 02:14:27 +0200

    feat: default board function implemented
    The defaultGame function was added to start a game on default settings.

--- src/player.js
+++ src/player.js
@@ -17,4 +17,21 @@ class Player {
         this._board = new Gameboard(boardSize);
         this._playerTypeFunctions = playerType;
     }
+
+    defaultGame() {
+        this._board.placeShip(6, 8, 0, 0, 3);
+        this._board.placeShip(1, 4, 1, 1, 4);
+        this._board.placeShip(8, 8, 2, 3, 2);
+        this._board.placeShip(2, 2, 3, 4, 2);
+        this._board.placeShip(5, 5, 3, 4, 2);
+        this._board.placeShip(2, 3, 6, 6, 2);
+        this._board.placeShip(6, 8, 6, 6, 3);
+        this._board.placeShip(5, 9, 8, 8, 5);
+    }
+
+    get gameboard() {
+        return this._board;
+    }
 }
+
+module.exports = {RealPlayer, ComputerPlayer, Player};
